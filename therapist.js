663b61846b098b110d47ad90fd5c941711ebc9bb
melissashayrobinson <melissa.shay.robinson@gmail.com>
2021-01-06 14:12:30 -0500

    Learning to organize. Working therapist.js file.

---
+++ therapist.js
@@ -0,0 +1,11 @@
+// A randomly generated message to promote self-healing. 
+// Inspired by and using the content of text messages from Dr. Nicole LePera @the.holistic.psychologist
+
+// I am ... and ...
+const adjectives = ['sturdy', 'creative', 'adaptable', 'clear-minded', 'beautiful', 'accepting', 'capable', 'compassionate', 'strong', 'kind', 'powerful', 'thoughtful', 'resilient', 'luminous'];
+
+// Today I will ...
+const directive = ['be willing to learn', 'just BE in the present moment', 'decide my own truth', 'not forget that I am living breathing magic', 'practice grace + self forgiveness', 'be concious of my breath', 'enjoy a hot shower and release the tension I am holding', 'get up and drink a glass of water'];
+
+// I know that ...
+const inspiration = ['everyone has something to teach me', 'personal responsibility is the foundation of freedom', 'I am worth being loved by myself', 'I can receive peace, love, and deeper awareness', 'sometimes I will need to be quiet + still for the answers to come', 'the person I admire, that *thing* they have that I wish I had – it\'s in me too', 'emotions flow through us. We are not our emotions, we experiencce them.', 'living an intentional life means making choices in alignment with who you wish to be + what you wish to experience', 'inner child wounding can create a scenario where two adults become two hurt children.']
